f9ea3d6e9882578445457723ac536cc51cfae2aa
Simone Lusenti <lusenti.s@gmail.com>
2019-05-07 15:37:05 +0200

    update page name mc-gowen => mcgowen

--- src/talks.js
+++ src/talks.js
@@ -133,7 +133,7 @@ module.exports = [
     workshop: false
   },
   {
-    key: 'bret-mc-gowen',
+    key: 'bret-mcgowen',
     bio: `Bret is a Developer Advocate for Serverless on the Google Cloud Platform team at Google, focusing on serverless products like Google Cloud Functions, App Engine, Firebase, machine learning APIs, and more. He's currently an aspiring Node.js developer. Prior to Google, Bret worked as a software engineer in the cloud industry at Rackspace. He's often on the running trail, volleyball court or kickball field. Bret earned a bachelor’s degree in computer science from Texas A&M University.`,
     photoUrl: 'bret-mcgowen.jpg',
     title: 'Serverless your containers with Knative plus Cloud Run',
