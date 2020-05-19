0de53993ad4a6922f4df979bd15cbb39cf0dd843
Aleksandra Iwończ <aleksandra.iwoncz@chaosgears.com>
2020-05-19 14:30:39 +0200

    Create table function changed

--- serverless/functions/create_table.py
+++ serverless/functions/create_table.py
@@ -6,22 +6,23 @@ from botocore.exceptions import ClientError
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 region = os.environ['REGION']
-table_name = os.environ['TABLE_NAME']
+table_registration = os.environ['TABLE_REGISTRATION']
+table_presentation = os.environ['TABLE_PRESENTATION']
 dynamodb = boto3.resource('dynamodb', region_name=region)
 
-def handler(event, context):
+def create_table(table_name, primary_key):
     try:
         table = dynamodb.create_table(
             TableName=table_name,
             KeySchema=[
                 {
-                    'AttributeName': 'registrationId',
+                    'AttributeName': primary_key,
                     'KeyType': 'HASH'  #Partition key
                 }
             ],
             AttributeDefinitions=[
                 {
-                    'AttributeName': 'registrationId',
+                    'AttributeName': primary_key,
                     'AttributeType': 'S'
                 }
             ],
@@ -37,6 +38,10 @@ def handler(event, context):
         print(err.response['Error']['Message'])
     
     print("Table status:", table.table_status)
+
+def handler(event, context):
+    #create_table(table_registration, 'registrationId')
+    create_table(table_presentation, 'presentationId')
 
             
        
